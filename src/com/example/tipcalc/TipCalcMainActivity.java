44931038436ea63a3dca9f43da8a7f547237249e
Simon Que <simonque@gmail.com>
2014-06-08 17:17:01 -0700

    Calculate tip from input field

--- src/com/example/tipcalc/TipCalcMainActivity.java
+++ src/com/example/tipcalc/TipCalcMainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class TipCalcMainActivity extends Activity {
@@ -32,8 +33,21 @@ public class TipCalcMainActivity extends Activity {
 			tip_percentage = 0;
 			break;
 		}
-		// TODO: Calculate tip later. For now, toast the amount.
-		Toast.makeText(getApplicationContext(), "" + tip_percentage, Toast.LENGTH_SHORT)
+		// Calculate tip:
+		float tip_value = getInputAmount() * tip_percentage / 100;
+		// TODO: Display tip later. For now, toast the amount.
+		Toast.makeText(getApplicationContext(), "" + tip_value, Toast.LENGTH_SHORT)
 			.show();
+	}
+
+	private float getInputAmount() {
+		EditText input_field = (EditText) findViewById(R.id.inputValue);
+		float value = 0.0f;
+		try {
+			value = Float.parseFloat(input_field.getText().toString());
+		} catch (NumberFormatException e) {
+			System.err.println("Caught exception: " + e.getMessage());
+		}
+		return value;
 	}
 }
