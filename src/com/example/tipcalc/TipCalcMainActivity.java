98635e4b96ed3ed0d6cffb5e1a03da0696c0b34c
Simon Que <simonque@gmail.com>
2014-06-08 17:02:35 -0700

    Listen for button clicks

--- src/com/example/tipcalc/TipCalcMainActivity.java
+++ src/com/example/tipcalc/TipCalcMainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class TipCalcMainActivity extends Activity {
 
@@ -11,5 +13,27 @@ public class TipCalcMainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tip_calc_main);
+	}
+
+	public void onButtonClick(View v) {
+		// Determine which button was clicked.
+		int tip_percentage = 0;
+		switch (v.getId()) {
+		case R.id.button10:
+			tip_percentage = 10;
+			break;
+		case R.id.button15:
+			tip_percentage = 15;
+			break;
+		case R.id.button20:
+			tip_percentage = 20;
+			break;
+		default:
+			tip_percentage = 0;
+			break;
+		}
+		// TODO: Calculate tip later. For now, toast the amount.
+		Toast.makeText(getApplicationContext(), "" + tip_percentage, Toast.LENGTH_SHORT)
+			.show();
 	}
 }
